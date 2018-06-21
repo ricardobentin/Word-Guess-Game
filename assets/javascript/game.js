4bc1dc73a6ee45fefc3b852c16e03e4cc606c4af
Nano Bacano <ricardo.bentin@gmail.com>
2018-06-21 15:55:49 -0400

    need to figure out why my image won't write to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var wordClues = ["Colombia", "Peru", "Brazil", ];
+var wordClues = ["Colombia", "Peru"];
 var word;
 var pictureClues = new Array();
     pictureClues[0] = new Image();
@@ -6,11 +6,11 @@ var pictureClues = new Array();
     pictureClues[1] = new Image();
     pictureClues[1].src = "./assets/images/Peru.jpg";
 var picture;
-    var computerChoice = [];
-    var wins = 0;
+var computerChoice = [];
+var wins = 0;
 var losses = 0;
 var guessesLeft= 9;
-
+//logic for the computer to guess between the items in wordClues and PictureClues arrays randomly
 var computerGuess = Math.floor(Math.random()* pictureClues.length);
 computerChoice.push(computerGuess);
 
@@ -21,3 +21,10 @@ picture = pictureClues[computerChoice];
 
 document.querySelector("#word").innerHTML = word;
 document.querySelector("#image").innerHTML = picture;
+
+//set a variable blanks to the length of the word that we need to blank out
+var blanks = word.length
+
+var blankReplace = []
+
+blankReplace.push(blanks)
